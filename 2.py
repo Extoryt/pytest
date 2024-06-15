8422f34b63cde69fab916ffa1d8a3bb3b357fba6
Extoryt <extoryt@gmail.com>
2024-06-15 12:20:39 +0200

    Project

---
+++ 2.py
@@ -0,0 +1,7 @@
+"""
+Zadanie 2:
+
+Przygotuj klasę Bank. Klasa powinna mieć możliwość wpłacania oraz wypłacania. W chwili inicjalizacji bank powinien zawierać puste saldo.
+
+Nie testuj sytuacji w której wypłacasz więcej niż to możliwe :)
+"""
